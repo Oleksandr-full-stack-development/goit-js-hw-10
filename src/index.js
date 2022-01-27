75d8c6dd9bf642329fcc5fee3b782fd56e630c29
Oleksandr Vaskevych <oleksandrvaskevich@gmail.com>
2022-01-27 10:31:24 +0200

    final files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import countryTpl from '../src/templates/country.hbs';
+import countriesTpl from '../src/templates/country-cards.hbs';
+import Notiflix from 'notiflix';
+import fetchCountriesByName from './fetchCountriesByName';
+
+
+const refs = {
+    countryContainer: document.querySelector('.country-info'),
+    searchBox: document.querySelector('#search-box'),
+    listCountry: document.querySelector('.country-list'),
+}
 
 const DEBOUNCE_DELAY = 300;
+
+refs.searchBox.addEventListener('input', debounce(onSearchInput,DEBOUNCE_DELAY));
+
+function onSearchInput (e) {
+    e.preventDefault();
+
+   let query = e.target.value.trim().toLowerCase();
+   fetchCountriesByName(query)
+        .then(createCountryCards)
+        .catch(onfetchError)
+        .finally(() => {refs.searchBox.value="";})
+
+    if (!query) {
+        refs.countryContainer.textContent='';
+        refs.listCountry.textContent='';
+    }
+}
+
+function createCountryCards (country) {
+    console.log(country);
+    
+    refs.countryContainer.textContent='';
+    refs.listCountry.textContent='';
+
+    if (country.length > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
+    }
+
+    if (country.length > 2 && country.length <10) {
+        const markupList = countriesTpl(country);
+        refs.listCountry.innerHTML = markupList;
+    }
+
+    if (country.length === 1) {
+        const markup = countryTpl(country);
+        refs.countryContainer.innerHTML = markup;
+    }
+    
+}
+
+
+function onfetchError(error) {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+}
+  
+
+
+
